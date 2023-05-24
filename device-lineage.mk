75c2aaa402f5892e80b7737acc7b5d65c0026bd4
AnierinB <anierin@evolution-x.org>
2023-05-24 15:53:09 -0700

    bluejay: Ship PixelParts
    Signed-off-by: AnierinB <anierin@evolution-x.org>

--- device-lineage.mk
+++ device-lineage.mk
@@ -13,6 +13,9 @@ DEVICE_PACKAGE_OVERLAYS += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.euicc.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.hardware.telephony.euicc.xml
 
+# PixelParts
+include packages/apps/PixelParts/device.mk
+
 # HBM
 PRODUCT_PACKAGES := \
     HbmSVManagerOverlay
